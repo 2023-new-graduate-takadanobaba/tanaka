6ee60c8a987b24624e8b3737c900ab1f94c95998
tanaka7438 <syouta.tanaka@goodsun-hd.com>
2023-06-22 15:50:38 +0900

    Update App.java

--- todo/src/App.java
+++ todo/src/App.java
@@ -1,5 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+System.out.println("数字を入力してください");
+ BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+ String str = reader.readLine();
+ 
+
+try {
+    int number = Integer.parseInt(str);
+   System.out.println( number + "が入力されました。");
+} catch (NumberFormatException e) {
+    System.out.println("数字ではありません");
+}
+
+
+
+
+
     }
 }
